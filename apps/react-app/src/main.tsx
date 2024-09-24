de188f55ff384ad091c329d82bc89b7700133d9c
Santiago Rendón <santirg2000@gmail.com>
2024-09-24 13:54:52 -0500

    feat: react app in single-spa

--- apps/react-app/src/main.tsx
+++ apps/react-app/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import singleSpaReact from "single-spa-react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root: React.FC = () => {
+  return <App />;
+};
+
+export const { bootstrap, mount, unmount } = singleSpaReact({
+  React,
+  ReactDOMClient,
+  rootComponent: root,
+  errorBoundary() {
+    // https://reactjs.org/docs/error-boundaries.html
+    return <div>This renders when a catastrophic error occurs</div>;
+  },
+});
--- apps/react-app/vite.config.ts
+++ apps/react-app/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: "src/main.tsx",
+      preserveEntrySignatures: "exports-only",
+      output: {
+        exports: "auto",
+        format: "system",
+        entryFileNames: "bundle.js",
+      },
+    },
+  },
   plugins: [react()],
-})
+  base: "http://localhost:3000",
+  preview: {
+    port: 3000,
+  },
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+});
